98caae77607fba59a4730007c0f3fa28343276f5
leahmarke <lmarke@live.com>
2022-08-12 20:04:24 -0400

    converted to main branch

--- index.js
+++ index.js
@@ -6,78 +6,22 @@ window.onload = function () {
   //  *Call functions on page load.
   // lookupDrink();
   getIngredientsList();
-  // searchByIngredient();
+  searchByIngredient();
 };
 
 // %%%%%%%%%%%%%%%%%%%%%% Lookup cocktail by cocktail ID %%%%%%%%%%%%%%%%%%%%%%
-const lookupDrink = () => {
+const lookupDrink = (drinkID) => {
+  drinkID = "15346";
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '0655aa903cmsh48b384669412764p17bd8fjsn60d6419ebf07',
-      'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
-    },
-  };
-
-  fetch('https://the-cocktail-db.p.rapidapi.com/lookup.php?i=12067', options)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      dataArray = data.drinks;
-      console.log('Cocktail by ID', dataArray);
-    })
-    .catch((err) => console.error(err));
-};
-
-// %%%%%%%%%%%%%%%%%%%%%%% Get A List of Ingredients %%%%%%%%%%%%%%%%%%%%%%%%%%
-const getIngredientsList = () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '0655aa903cmsh48b384669412764p17bd8fjsn60d6419ebf07',
-      'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
-    },
-  };
-
-  fetch('https://the-cocktail-db.p.rapidapi.com/list.php?i=list', options)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      dataArray = data.drinks;
-      console.log('List of Ingredients', dataArray);
-      // * Creating an array of all the ingredients.
-      const arrayOfIngredients = dataArray.map(
-        (ingredient) => ingredient.strIngredient1
-      );
-      //* Adding arrayOfIngredients to dropdown menu
-      const ingredientDropdown = document.getElementById('ingredient-list');
-      arrayOfIngredients.forEach((ingredient) => {
-        ingredientDropdown.innerHTML += `<option >${ingredient}</option>`;
-      });
-    })
-    .catch((err) => console.error(err));
-};
-
-// %%%%%%%%%%%%%%%%%%%%%%% Search by Ingredients %%%%%%%%%%%%%%%%%%%%%%%%%%
-const searchByIngredient = () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '0655aa903cmsh48b384669412764p17bd8fjsn60d6419ebf07',
-      'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
+      "X-RapidAPI-Key": "0655aa903cmsh48b384669412764p17bd8fjsn60d6419ebf07",
+      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
     },
   };
 
   fetch(
-    'https://the-cocktail-db.p.rapidapi.com/filter.php?i=Club%20Soda',
+    `https://the-cocktail-db.p.rapidapi.com/lookup.php?i=${drinkID}`,
     options
   )
     .then((response) => {
@@ -88,7 +32,73 @@ const searchByIngredient = () => {
     })
     .then((data) => {
       dataArray = data.drinks;
-      console.log('Search Ingredients', dataArray);
+      console.log("Cocktail by ID", dataArray);
+    })
+    .catch((err) => console.error(err));
+};
+
+// %%%%%%%%%%%%%%%%%%%%%%% Get A List of Ingredients %%%%%%%%%%%%%%%%%%%%%%%%%%
+const getIngredientsList = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "0655aa903cmsh48b384669412764p17bd8fjsn60d6419ebf07",
+      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
+    },
+  };
+
+  fetch("https://the-cocktail-db.p.rapidapi.com/list.php?i=list", options)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      dataArray = data.drinks;
+      console.log("List of Ingredients", dataArray);
+      // * Creating an array of all the ingredients.
+      const arrayOfIngredients = dataArray.map(
+        (ingredient) => ingredient.strIngredient1
+      );
+      //* Adding arrayOfIngredients to dropdown menu
+      const ingredientDropdown = document.getElementById("ingredient-list");
+      arrayOfIngredients.forEach((ingredient) => {
+        ingredientDropdown.innerHTML += `<option >${ingredient}</option>`;
+      });
+    })
+    .catch((err) => console.error(err));
+};
+
+// %%%%%%%%%%%%%%%%%%%%%%% Search by Ingredients %%%%%%%%%%%%%%%%%%%%%%%%%%
+// getting ingredient from dropdown menu
+const searchByIngredient = (ingredient) => {
+  ingredient = document.getElementById("ingredient-list");
+  const specifiedIngredient = ingredient.options[ingredient.selectedIndex].text;
+  console.log(ingredient);
+  console.log(`The selected ingredient is: ${specifiedIngredient}`);
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "0655aa903cmsh48b384669412764p17bd8fjsn60d6419ebf07",
+      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    `https://the-cocktail-db.p.rapidapi.com/filter.php?i=${specifiedIngredient}`,
+    options
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      dataArray = data.drinks;
+      console.log("Search by Ingredients", dataArray);
     })
     .catch((err) => console.error(err));
 };
